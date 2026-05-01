1a83d7a7c42c1e78fd3ddbe265b19857c455c1f8
dnatisdale <dant.grnt@gmail.com>
2026-05-01 14:03:18 -0700

    splashscreen

--- src/App.tsx
+++ src/App.tsx
@@ -779,8 +779,8 @@ export default function App() {
   };
 
   useEffect(() => {
-    const timer1 = setTimeout(() => setSplashState('fading'), 1500);
-    const timer2 = setTimeout(() => setSplashState('hidden'), 2000);
+    const timer1 = setTimeout(() => setSplashState('fading'), 7500);
+    const timer2 = setTimeout(() => setSplashState('hidden'), 8500);
     return () => { clearTimeout(timer1); clearTimeout(timer2); };
   }, []);
 
