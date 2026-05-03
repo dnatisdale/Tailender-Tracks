3a86ed9361f48a4ea318f36eb2803a110f533f49
dnatisdale <dant.grnt@gmail.com>
2026-05-02 18:44:35 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -701,8 +701,8 @@ function SettingsView({ deferredPrompt, onInstallClick }: { deferredPrompt: any;
         )}
       </div>
 
-      <div style={{ textAlign: 'center', marginTop: 32, opacity: 0.0 }}>
-        <img src={`${import.meta.env.BASE_URL}icons/Boot3.png`} alt="Logo" style={{ height: 90 }} />
+      <div style={{ textAlign: 'center', marginTop: 32, opacity: 1.0 }}>
+        <img src={`${import.meta.env.BASE_URL}icons/Boot3.png`} alt="Logo" style={{ height: 20 }} />
         <p style={{ margin: '8px 0 0', fontSize: '0.8rem' }}>Tailender Tracks v1.0.0<br/>78 RPM Series</p>
       </div>
     </div>
